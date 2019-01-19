ce880c377adee6748c2762fd216f5c0cb196747d
Saibharath.gudimella <Saibharath.gudimella@GGKU4SYS89>
2019-01-19 15:05:25 +0530

    Add project files.

---
+++ AzureImageUploadDemo/Controllers/ImageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureImageUploadDemo.Controllers
+{
+    public class ImageController : Controller
+    {
+        ImageService imageService = new ImageService();
+        // GET: Image
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase photo)
+        {
+            var imageUrl = await imageService.ImageUpload(photo);
+            TempData["LatestImage"] = imageUrl.ToString();
+            return RedirectToAction("LatestImage");
+        }
+
+        public ActionResult LatestImage()
+        {
+            var latestImage = string.Empty;
+            if (TempData["LatestImage"] != null)
+            {
+                ViewBag.LatestImage = Convert.ToString(TempData["LatestImage"]);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AzureImageUploadDemo/ImageService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System.IO;
+
+namespace AzureImageUploadDemo
+{
+    public class ImageService
+    {
+        public async Task<string> ImageUpload(HttpPostedFileBase image)
+        {
+            string imagepath = null;
+            try
+            {
+                StorageCredentials storageCredentials = new StorageCredentials("azutetestblob2065", "Ew+8lF5ACjxNZzr6j292eMloOZiARQgYNMdrNvghO3Eey8uRlsIwTVAvcp05zXJEI7oroyJREIzjJm1zi+gOlw==");
+                CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
+                CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference("blobstoragedemo");
+                if (await cloudBlobContainer.CreateIfNotExistsAsync())
+                {
+                    await cloudBlobContainer.SetPermissionsAsync(
+                        new BlobContainerPermissions
+                        {
+                            PublicAccess = BlobContainerPublicAccessType.Blob
+                        }
+                        );
+                }
+                string imageName = Guid.NewGuid().ToString() + "-" + Path.GetExtension(image.FileName);
+
+                CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(imageName);
+                cloudBlockBlob.Properties.ContentType = image.ContentType;
+                await cloudBlockBlob.UploadFromStreamAsync(image.InputStream);
+
+                imagepath = cloudBlockBlob.Uri.ToString();
+            
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return imagepath;
+        }
+    }
+}
